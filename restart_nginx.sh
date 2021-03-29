b5fb9b77d954a3fee78fd2393148b48102d1ba57
sedric <sedric@student.21-school.ru>
2021-03-29 23:27:49 +0300

    except gravafa

--- restart_nginx.sh
+++
@@ -1,5 +0,0 @@
-kubectl delete svc nginx-svc
-kubectl delete deploy nginx-deployment
-
-docker build -t nginx ./srcs/nginx
-kubectl apply -f ./srcs/nginx/nginx.yaml
--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-minikube start --driver=virtualbox --memory='2000' --disk-size 2000MB;
+minikube start --vm-driver=docker
 
 eval $(minikube docker-env)
 minikube addons enable metallb
@@ -10,11 +10,14 @@ docker build -t nginx ./srcs/nginx
 docker build -t mysql ./srcs/mysql
 docker build -t phpmyadmin ./srcs/phpmyadmin
 docker build -t ftps ./srcs/ftps
+docker build -t wordpress ./srcs/wordpress
 
-kubectl apply -f ./srcs/metallb.yaml
+kubectl apply -f ./srcs/stuff/metallb.yaml
+kubectl apply -f ./srcs/stuff/storage.yaml
 kubectl apply -f ./srcs/mysql/mysql.yaml
 kubectl apply -f ./srcs/ftps/ftps.yaml
 kubectl apply -f ./srcs/phpmyadmin/phpmyadmin.yaml
 kubectl apply -f ./srcs/nginx/nginx.yaml
+kubectl apply -f ./srcs/wordpress/wordpress.yaml
 
 minikube dashboard
